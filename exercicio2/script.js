4a8fc89f29047cf8b6d9dfecd74c0751f6a9dc15
ludmilajf <ludmiladjf@gmail.com>
2022-09-12 15:41:42 -0300

    Exercíos resolvidos

---
+++ exercicio2/script.js
@@ -0,0 +1,5 @@
+let userInput = Number(prompt("Insira um número"))
+
+for(let i = 1; i<=10; i++){
+    console.log(`${userInput} x ${i} = ${i * userInput}`)
+}
---
+++ exercicio3/script.js
@@ -0,0 +1,6 @@
+let maioresPaises = ["Rússia", "Canadá", "China", "EUA", "Brasil"]
+
+for(let i=0; i<5; i++){
+    console.log(`${i+1}º - ${maioresPaises[i]}
+    `)
+}
